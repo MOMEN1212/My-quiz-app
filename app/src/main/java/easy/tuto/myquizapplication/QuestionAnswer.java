bca09b2b0299e1a8a45a54253ccb7f71c5d82a17
MOMEN1212 <moomen133@gmail.com>
2024-04-14 19:37:31 +0300

    finished project

---
+++ app/src/main/java/easy/tuto/myquizapplication/QuestionAnswer.java
@@ -0,0 +1,26 @@
+package easy.tuto.myquizapplication;
+
+public class QuestionAnswer {
+
+    public static String question[] ={
+            "what is the best university in turkey?",
+            "what is the best subject of this semester?",
+            "what is the best programming language?",
+            "what is the best car company?"
+    };
+
+    public static String choices[][] = {
+            {"Okan","Maltepe","Medipol","Marmara"},
+            {"Mobile programming","Math1","physics","chemistry"},
+            {"csharp","c++","python","Java"},
+            {"BMW","Toyota","benz","Tesla"}
+    };
+
+    public static String correctAnswers[] = {
+            "Okan",
+            "Mobile programming",
+            "python",
+            "Tesla"
+    };
+
+}
